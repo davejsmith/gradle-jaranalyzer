ad533ebddd46596bd5dc49ea4af967e6cc7c2808
smith <smith@nwoca.org>
2013-06-13 04:48:54 +0100

    added html report and apache 2.0 license

--- src/main/groovy/me/davesmith/gradle/plugins/jaranalyzer/JarAnalyzerReportTask.groovy
+++ src/main/groovy/me/davesmith/gradle/plugins/jaranalyzer/JarAnalyzerReportTask.groovy
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package me.davesmith.gradle.plugins.jaranalyzer
 
 import com.kirkk.analyzer.textui.Summary
@@ -19,6 +35,9 @@ class JarAnalyzerReportTask extends DefaultTask {
     @OutputFile
     File xmlReport = new File(outputDir,'jaranalyzer.xml')
 
+    @OutputFile
+    File htmlReport = new File(outputDir,'jaranalyzer.html')
+
     @TaskAction
     def analyze() {
 
@@ -34,9 +53,16 @@ class JarAnalyzerReportTask extends DefaultTask {
         if (jarAnalyzerExtension.xml) {
             Summary summary = new XMLUISummary()
             summary.createSummary(jarDir, xmlReport, jarAnalyzerExtension.packageFilter, jarAnalyzerExtension.jarFilter);
+        }
 
+        if (jarAnalyzerExtension.html) {
+            ant.xslt(in: xmlReport,  out:htmlReport) {
+                style {
+                    javaresource(name: "com/kirkk/analyzer/jaranalyzer.xsl",
+                            classpath: project.buildscript.configurations.classpath.asPath )
+                }
+            }
         }
     }
-
 
 }
